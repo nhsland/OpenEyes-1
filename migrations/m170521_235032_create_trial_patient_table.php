bae2acfd28b9a8a5b9756caa488309ac4d2459d3
Liam Marshall <marshl@live.com.au>
2017-05-22 14:08:05 +1000

    Added the patient_status column to the trial_patient table migration script

--- migrations/m170521_235032_create_trial_patient_table.php
+++ migrations/m170521_235032_create_trial_patient_table.php
@@ -9,6 +9,7 @@ class m170521_235032_create_trial_patient_table extends CDbMigration
             'external_trial_identifier' => 'varchar(64) collate utf8_bin',
             'trial_id' => 'int(10) unsigned NOT NULL',
             'patient_id' => 'int(10) unsigned NOT NULL',
+            'patient_status' => 'int(10) unsigned NOT NULL',
             'last_modified_date' => 'datetime NOT NULL DEFAULT \'1900-01-01 00:00:00\'',
             'last_modified_user_id' => 'int(10) unsigned NOT NULL DEFAULT \'1\'',
             'created_user_id' => 'int(10) unsigned NOT NULL DEFAULT \'1\'',
