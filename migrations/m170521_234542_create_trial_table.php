b50d3d794f54d279b13be621f058b6ed3649e344
Liam Marshall <marshl@live.com.au>
2017-05-29 14:37:40 +1000

    Changed the create_trial and create_trial_patient migration scripts to use OEMIgration instead of CDbMigration

--- migrations/m170521_234542_create_trial_table.php
+++ migrations/m170521_234542_create_trial_table.php
@@ -1,32 +1,25 @@
 <?php
 
-class m170521_234542_create_trial_table extends CDbMigration
+class m170521_234542_create_trial_table extends OEMigration
 {
+    const VERSIONED = true;
+
     public function up()
     {
-        $this->createTable('trial', array(
-            'id' => 'int(10) unsigned NOT NULL AUTO_INCREMENT',
+        $this->createOETable('trial', array(
+            'id' => 'pk',
             'name' => 'varchar(64) collate utf8_bin NOT NULL',
             'description' => 'varchar(64) collate utf8_bin',
             'owner_user_id' => 'int(10) unsigned NOT NULL',
             'status' => 'int(10) unsigned NOT NULL',
-            'last_modified_date' => 'datetime NOT NULL DEFAULT \'1900-01-01 00:00:00\'',
-            'last_modified_user_id' => 'int(10) unsigned NOT NULL DEFAULT \'1\'',
-            'created_user_id' => 'int(10) unsigned NOT NULL DEFAULT \'1\'',
-            'created_date' => 'datetime NOT NULL DEFAULT \'1900-01-01 00:00:00\'',
-            'PRIMARY KEY (`id`)',
-            'KEY `trial_owner_user_id_fk` (`owner_user_id`)',
-            'KEY `trial_last_modified_user_id_fk` (`last_modified_user_id`)',
-            'KEY `trial_created_user_id_fk` (`created_user_id`)',
-            'CONSTRAINT `trial_owner_user_id_fk` FOREIGN KEY (`owner_user_id`) REFERENCES `user` (`id`)',
-            'CONSTRAINT `trial_created_user_id_fk` FOREIGN KEY (`created_user_id`) REFERENCES `user` (`id`)',
-            'CONSTRAINT `trial_last_modified_user_id_fk` FOREIGN KEY (`last_modified_user_id`) REFERENCES `user` (`id`)',
-        ), 'ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin'
+        ), self::VERSIONED
         );
+
+        $this->addForeignKey('trial_owner_fk', 'trial', 'owner_user_id', 'user', 'id');
     }
 
     public function down()
     {
-        $this->dropTable('trial');
+        $this->dropOETable('trial', self::VERSIONED);
     }
 }
