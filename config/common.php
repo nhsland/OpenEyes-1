bbb58c0153b6f6fe54f36f63d753d06b44e45127
Liam Marshall <marshl@live.com.au>
2017-06-02 09:31:52 +1000

    Added a link to the Trial page underneath the More section of the menu bar

---
+++ config/common.php
@@ -0,0 +1,17 @@
+<?php
+
+return array(
+    'params' => array(
+        'menu_bar_items' => array(
+            'admin' => array(
+                'sub' => array(
+                    'trials' => array(
+                        'title' => 'Trials',
+                        'uri' => 'OETrial',
+                        'position' => 50,
+                    ),
+                ),
+            ),
+        ),
+    ),
+);
