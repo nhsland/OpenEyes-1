90ae22866649c7476a925adbe3ffc18d59193050
Liam Marshall <marshl@live.com.au>
2017-08-16 10:56:04 +1000

    Improved the appearance of the fields in the trial form

--- views/trial/_form.php
+++ views/trial/_form.php
@@ -16,28 +16,32 @@
     <?php echo $form->errorSummary($model); ?>
 
   <div class="row field-row">
-    <div class="large-6 column">
-      <div class="row field-row">
-          <?php echo $form->labelEx($model, 'name'); ?>
-          <?php echo $form->textField($model, 'name', array('size' => 64, 'maxlength' => 64)); ?>
-          <?php echo $form->error($model, 'name'); ?>
-      </div>
+    <div class="large-2 column">
+        <?php echo $form->labelEx($model, 'name'); ?>
+    </div>
+    <div class="large-6 column end">
+        <?php echo $form->textField($model, 'name', array('size' => 64, 'maxlength' => 64)); ?>
+        <?php echo $form->error($model, 'name'); ?>
     </div>
   </div>
   <div class="row field-row">
-    <div class="large-6 column">
-      <div class="row field-row">
-          <?php echo $form->labelEx($model, 'external_data_link'); ?>
-          <?php echo $form->urlField($model, 'external_data_link',
-              array('size' => 100, 'maxlength' => 255, 'onblur' => 'checkUrl(this)')); ?>
-          <?php echo $form->error($model, 'external_data_link'); ?>
-      </div>
+    <div class="large-2 column">
+        <?php echo $form->labelEx($model, 'external_data_link'); ?>
+    </div>
+    <div class="large-6 column end">
+        <?php echo $form->urlField($model, 'external_data_link',
+            array('size' => 100, 'maxlength' => 255, 'onblur' => 'checkUrl(this)')); ?>
+        <?php echo $form->error($model, 'external_data_link'); ?>
     </div>
   </div>
   <div class="row field-row">
-      <?php echo $form->labelEx($model, 'description'); ?>
-      <?php echo $form->textArea($model, 'description'); ?>
-      <?php echo $form->error($model, 'description'); ?>
+    <div class="large-2 column">
+        <?php echo $form->labelEx($model, 'description'); ?>
+    </div>
+    <div class="large-6 column end">
+        <?php echo $form->textArea($model, 'description'); ?>
+        <?php echo $form->error($model, 'description'); ?>
+    </div>
   </div>
 
   <div class="row field-row">
@@ -54,7 +58,7 @@
         <?php echo $form->textField($model, 'started_date'); ?>
         <?php echo $form->error($model, 'started_date'); ?>
     </div>
-    <div class="large-3 column end"><label><i>(dd/mm/yyyy)</i></label></div>
+    <div class="large-2 column end"><label><i>(dd/mm/yyyy)</i></label></div>
   </div>
 
   <div class="row field-row">
@@ -71,27 +75,22 @@
         <?php echo $form->textField($model, 'closed_date'); ?>
         <?php echo $form->error($model, 'closed_date'); ?>
     </div>
-    <div class="large-3 column end"><label><i>(dd/mm/yyyy)</i></label></div>
+    <div class="large-2 column end"><label><i>(dd/mm/yyyy)</i></label></div>
   </div>
 
   <div class="row field-row">
-    <div class="large-6 column">
-      <div class="row field-row">
-        <div class="large-3 column">
-            <?php echo $form->labelEx($model, 'trial_type'); ?>
-        </div>
-        <div class="large-6 column end">
-            <?php foreach (Trial::model()->getTrialTypeOptions() as $trial_type => $type_label): ?>
-              <div class="row field-row text">
-                <label>
-                    <?php echo $form->radioButton($model, 'trial_type',
-                        array('value' => $trial_type, 'uncheckValue' => null)); ?>
-                    <?php echo $type_label; ?>
-                </label>
-              </div>
-            <?php endforeach; ?>
-        </div>
-      </div>
+    <div class="large-2 column">
+        <?php echo $form->labelEx($model, 'trial_type'); ?>
+    </div>
+    <div class="large-2 column end">
+        <?php foreach (Trial::model()->getTrialTypeOptions() as $trial_type => $type_label): ?>
+
+          <label>
+              <?php echo $form->radioButton($model, 'trial_type',
+                  array('value' => $trial_type, 'uncheckValue' => null)); ?>
+              <?php echo $type_label; ?>
+          </label>
+        <?php endforeach; ?>
     </div>
   </div>
 
